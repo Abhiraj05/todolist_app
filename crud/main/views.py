62ca128a37d3ba0fdbd64a38dff9445e8dff34c7
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-08-13 19:55:03 +0530

    connected db and created models

--- crud/main/views.py
+++ crud/main/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from main.models import Add_Task,User_Registration
+from django.contrib import messages
 # Create your views here.
-def index(request):
-    if request.method == "POST":
-        taskname=request.POST.get("taskname")
-        return HttpResponse(f"task:{taskname}")
+# def index(request):
+#     if request.method == "POST":
+#         taskname=request.POST.get("taskname")
+#         return HttpResponse(f"task:{taskname}")
+#     return render(request,"home.html")
+
+def add_task(request):
+    if request.method=="POST":
+        form=Add_Task(request.POST)
+        task_name=request.POST.get('')
+        # user_id=
+        print(request.POST)
+        task=Add_Task.objects.create(
+            task_name=task_name
+            
+        )
+        form.save()
+        messages.success(request,"task is added successfully")
+        return redirect("")
     return render(request,"home.html")
