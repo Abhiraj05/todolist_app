c8b700a27b6994583a6b434e4024c9d5c8bbb6e7
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-08-13 19:58:00 +0530

    connected db and created models

--- crud/main/views.py
+++ crud/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from main.models import Add_Task,User_Registration
+from main.models import Add_Task, User_Registration
 from django.contrib import messages
 # Create your views here.
 # def index(request):
@@ -9,17 +9,24 @@ from django.contrib import messages
 #         return HttpResponse(f"task:{taskname}")
 #     return render(request,"home.html")
 
+
+def user_registeration(request):
+    if request.method == "POST":
+        username = models.CharField(max_length=100, null=False, blank=False)
+        password = models.CharField(max_length=10
+
+
 def add_task(request):
-    if request.method=="POST":
-        form=Add_Task(request.POST)
-        task_name=request.POST.get('')
+    if request.method == "POST":
+        form = Add_Task(request.POST)
+        task_name = request.POST.get('')
         # user_id=
         print(request.POST)
-        task=Add_Task.objects.create(
+        task = Add_Task.objects.create(
             task_name=task_name
-            
+
         )
         form.save()
-        messages.success(request,"task is added successfully")
+        messages.success(request, "task is added successfully")
         return redirect("")
-    return render(request,"home.html")
+    return render(request, "home.html")
