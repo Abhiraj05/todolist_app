5f2a918e68158c08ce9a4526fa3ab4145ed4eb1a
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-08-13 18:44:24 +0530

    connected db and created models

--- crud/main/models.py
+++ crud/main/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
 class User_Registration(models.Model):
-    username=models.CharField(max_length=100,null=False,blank=False)
-    password=models.CharField(max_length=10,null=False,blank=False)
+    username = models.CharField(max_length=100, null=False, blank=False)
+    password = models.CharField(max_length=10, null=False, blank=False)
+
 
 class Add_Task(models.Model):
-    task_name=models.CharField(max_length=100,null=False,blank=False)
-    user=models.ForeignKey(User_Registration,on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=500, null=False, blank=False)
+    user = models.ForeignKey(User_Registration, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    task_done = models.IntegerField(default=False)
