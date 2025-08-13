53b1b48946dfb7ec427a36365cfcb48090538fc0
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-08-13 17:11:34 +0530

    connected db and created models

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# mysql databse connetion
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 
 
 # Password validation
--- crud/main/models.py
+++ crud/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User_Registration(models.Model):
+    username=models.CharField(max_length=100,null=False,blank=False)
+    password=models.CharField(max_length=10,null=False,blank=False)
+
+class Add_Task(models.Model):
+    task_name=models.CharField(max_length=100,null=False,blank=False)
+    user=models.ForeignKey(User_Registration,on_delete=models.CASCADE)
